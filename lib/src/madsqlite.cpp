d9a3dbb8f805721025c8b6837f2ff70ce449d0e3
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2024-03-07 22:23:09 +0100

    feat: parse values to sqlite types

--- lib/src/madsqlite.cpp
+++ lib/src/madsqlite.cpp
@@ -2,8 +2,7 @@
 #include "gc.h"
 #include "apply-pap.hpp"
 #include "list.hpp"
-#include <string.h>
-#include <stdio.h>
+#include "number.hpp"
 
 // Sqlite errors
 
@@ -45,6 +44,17 @@ extern "C"
 {
 #endif
 
+  const int64_t madsqlite__Value_Blob = 0;
+  const int64_t madsqlite__Value_Integer = 1;
+  const int64_t madsqlite__Value_Null = 2;
+  const int64_t madsqlite__Value_Real = 3;
+  const int64_t madsqlite__Value_Text = 4;
+
+  typedef struct madsqlite__Value {
+    int64_t index;
+    void *data;
+  } madsqlite__Value_t;
+
   void madlibsqlite__connect(char *path, PAP_t *callback) {
     sqlite3 *db;
     int64_t error = sqlite3_open(path, &db);
@@ -71,18 +81,93 @@ extern "C"
     return 0;
   }
 
-  int madlibsqlite__query(sqlite3 *db, char *sql, PAP_t *callback, PAP_t *errorCallback) {
+
+  // #define SQLITE_INTEGER  1
+  // #define SQLITE_FLOAT    2
+  // #define SQLITE_TEXT     3
+  // #define SQLITE_BLOB     4
+  // #define SQLITE_NULL     5
+
+  // export type SqliteType
+  // = BlobType(String)      0
+  // | IntegerType(Integer)  1
+  // | NullType              2
+  // | RealType(Float)       3
+  // | TextType(String)      4
+
+  void madlibsqlite__query(sqlite3 *db, char *sql, PAP_t *callback, PAP_t *errorCallback) {
     char *error = 0;
-    int64_t r = sqlite3_exec(db, (const char *)sql, onRowRead, (void *)callback, &error);
+    sqlite3_stmt *pstmt;
+
+    int r = sqlite3_prepare_v3(db, sql, -1, 0, &pstmt, NULL);
 
     if (r != 0) {
+      error = (char *) sqlite3_errmsg(db);
       size_t errorMessageSize = strlen(error);
       char *errorMessage = (char *)GC_MALLOC(errorMessageSize + 1);
       memcpy(errorMessage, error, errorMessageSize);
       errorMessage[errorMessageSize] = '\0';
       __applyPAP__(errorCallback, 2, r, errorMessage);
+      return;
     }
-    return r;
+
+    madlib__list__Node_t *rows = NULL, *tail = NULL;
+    while(sqlite3_step(pstmt) == SQLITE_ROW) {
+      madlib__list__Node_t *row = madlib__list__empty();
+      int columnCount = sqlite3_column_count(pstmt);
+
+      for (int i = columnCount - 1; i >= 0; i--) {
+        madsqlite__Value_t *value = (madsqlite__Value_t *) GC_MALLOC(sizeof(madsqlite__Value_t));
+        int colType = sqlite3_column_type(pstmt, i);
+          switch(colType) {
+            case SQLITE_INTEGER: {
+              value->index = madsqlite__Value_Integer;
+              value->data = (void *) sqlite3_column_int64(pstmt, i);
+              break;
+            }
+
+            case SQLITE_FLOAT: {
+              value->index = madsqlite__Value_Real;
+              value->data = boxDouble(sqlite3_column_double(pstmt, i));
+              break;
+            }
+
+            case SQLITE_TEXT:
+            case SQLITE_BLOB: {
+              char *colValue = (char *) sqlite3_column_text(pstmt, i);
+              size_t valueSize = strlen(colValue);
+              char *text = (char *)GC_MALLOC(valueSize + 1);
+              memcpy(text, colValue, valueSize);
+              text[valueSize] = '\0';
+
+              value->index = madsqlite__Value_Text;
+              value->data = text;
+              break;
+            }
+
+            default: {
+              value->index = madsqlite__Value_Null;
+            }
+          }
+        row = madlib__list__internal__push(value, row);
+      }
+
+      madlib__list__Node_t *node = madlib__list__singleton(row);
+      if (rows == NULL) {
+        rows = node;
+        tail = node;
+      } else {
+        tail->next = node;
+        tail = node;
+      }
+    }
+    sqlite3_finalize(pstmt);
+
+    if (rows == NULL) {
+      rows = madlib__list__empty();
+    }
+
+    __applyPAP__(callback, 1, rows);
   }
 
 #ifdef __cplusplus
